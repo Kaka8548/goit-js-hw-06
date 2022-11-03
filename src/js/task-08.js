c434043dfedf7362b60892c0da9e2b724ae78f54
Kaka8548 <katyakatya8548@gmail.com>
2022-11-03 23:06:25 +0400

    Зроблено ДЗ8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
+const userData = {};
 
+const handleSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.target;
+  if (email.value && password.value) {
+    userData[email.name] = email.value;
+    userData[password.name] = password.value;
+    console.log(userData);
+    event.currentTarget.reset();
+  } else {
+    alert('Всі поля повинні бути заповнені 🙃');
+  }
+};
+
+formEl.addEventListener('submit', handleSubmit);
