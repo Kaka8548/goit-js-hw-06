377490a4e7374fcd929c950ece74f17caa5729ed
Kaka8548 <katyakatya8548@gmail.com>
2022-11-04 00:21:09 +0400

    Зроблено ДЗ10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,40 @@
-function getRandomHexColor() {
+const boxesEl = document.querySelector('#boxes');
+const buttonsEl = document.querySelector('#controls');
+const inputEl = buttonsEl.firstElementChild;
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
+
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const createBoxes = amount => {
+  const boxArr = [];
+  let size = 30;
+  for (let i = 1; i <= Number(amount); i += 1) {
+    let randomColor = getRandomHexColor();
+    const box = `<div style='background-color:${randomColor}; width:${size}px; height:${size}px'></div>`;
+    boxArr.push(box);
+    size += 10;
+  }
+  addBoxes(boxArr);
+};
+
+const addBoxes = arr => {
+  boxesEl.insertAdjacentHTML('afterbegin', arr.join(''));
+};
+
+const onCreateClick = event => {
+  event.preventDefault();
+  boxesEl.innerHTML = '';
+  createBoxes(inputEl.value);
+};
+
+const onDestroyClick = event => {
+  event.preventDefault();
+  boxesEl.innerHTML = '';
+  inputEl.value = '';
+};
+
+createBtnEl.addEventListener('click', onCreateClick);
+destroyBtnEl.addEventListener('click', onDestroyClick);
