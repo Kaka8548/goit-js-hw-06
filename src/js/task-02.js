948025baaec467836991db0d81658105e7ab6686
Kaka8548 <katyakatya8548@gmail.com>
2022-11-03 20:38:15 +0400

    Зроблено ДЗ2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ingredientsListArr = [];
+
+const makeIngridientList = ingredients.map(el => {
+  const ingredient = document.createElement('li');
+  ingredient.textContent = el;
+  ingredient.classList.add('item');
+
+  ingredientsListArr.push(ingredient);
+});
+
+const ingredientsListEl = document.querySelector('#ingredients');
+ingredientsListEl.append(...ingredientsListArr);
