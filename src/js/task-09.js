1fe4ecd7db68e4acee09d378caf83b35c8ea2135
Kaka8548 <katyakatya8548@gmail.com>
2022-11-03 23:19:10 +0400

    Зроблено ДЗ9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const changeColorBtn = document.querySelector('button.change-color');
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const changeColorValue = newColor => {
+  const textEl = document.querySelector('span.color');
+  textEl.innerHTML = newColor;
+};
+
+const changeBgColor = () => {
+  const bodyEl = document.querySelector('body');
+  let newColor = getRandomHexColor();
+  console.log(newColor);
+  bodyEl.style.backgroundColor = newColor;
+  changeColorValue(newColor);
+};
+
+changeColorBtn.addEventListener('click', changeBgColor);
